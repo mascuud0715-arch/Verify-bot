55b04d0c0f5854f7dbaafb0c72b8720362707f06
mascuud0715-arch <mascuud0715@gmail.com>
2026-03-15 10:16:13 +0300

    Create verify_bot.py

---
+++ verify_bot.py
@@ -0,0 +1,23 @@
+import telebot
+import random
+import os
+
+TOKEN = os.getenv("VERIFY_BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+codes = {}
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+
+    code = random.randint(10000,99999)
+
+    codes[msg.from_user.id] = code
+
+    bot.send_message(
+        msg.chat.id,
+        f"🔐 Your Verify Code:\n\n{code}\n\nKu celi botkii ku waydiiyay verify."
+    )
+
+bot.infinity_polling()
