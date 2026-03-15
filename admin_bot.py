e3d44aa6b30cc2754ec4612dc4560ec52d3a881d
mascuud0715-arch <mascuud0715@gmail.com>
2026-03-15 11:36:28 +0300

    Update admin_bot.py

--- admin_bot.py
+++ admin_bot.py
@@ -12,53 +12,19 @@ def load_bots():
         with open("bots.json") as f:
             return json.load(f)
     except:
-        return {}
-
-def load_users():
-    try:
-        with open("users.json") as f:
-            return json.load(f)
-    except:
         return []
 
-@bot.message_handler(commands=['start'])
-def start(msg):
+@bot.message_handler(commands=["start"])
+def admin(message):
 
-    if msg.from_user.id!=ADMIN_ID:
+    if message.from_user.id != ADMIN_ID:
         return
+
+    bots = load_bots()
 
     bot.send_message(
-        msg.chat.id,
-        "/stats\n/bots"
+        message.chat.id,
+        f"🤖 Total Bots: {len(bots)}"
     )
-
-@bot.message_handler(commands=['stats'])
-def stats(msg):
-
-    if msg.from_user.id!=ADMIN_ID:
-        return
-
-    bots=load_bots()
-    users=load_users()
-
-    bot.send_message(
-        msg.chat.id,
-        f"Bots: {len(bots)}\nUsers: {len(users)}"
-    )
-
-@bot.message_handler(commands=['bots'])
-def bots(msg):
-
-    if msg.from_user.id!=ADMIN_ID:
-        return
-
-    data=load_bots()
-
-    text="Bots:\n\n"
-
-    for b in data.values():
-        text+=f"@{b['username']}\n"
-
-    bot.send_message(msg.chat.id,text)
 
 bot.infinity_polling()
